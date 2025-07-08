388afd532ed2ca757f3925ae71700e23f3450fd4
sanyamsinghal3 <sanyamsinghal3@gmail.com>
2025-07-08 12:22:30 +0530

    worked on create http server using node js

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ What is nodeJs
 nodejs is open source, cross paltform, javascript run time envirnoment using this we can execute javascript code outside browser or without browser most imporatnt server side. It is built on Chrome's V8 JavaScript engine
 
 
-Day 1
+# Day 1
 javascript module using es6
 
 in package.json file "type = module" -- tells Node.js to treat your .js files as ES modules. without this Default = CommonJS.
@@ -36,7 +36,7 @@ function add(a,b) {
 }   1q2`
 module.exports=add;
 
-Day 2
+# Day 2
  learn file system (two way tradional and es6)
 To handle file operations like creating, reading, deleting, etc., Node.js provides an inbuilt module called FS (File System).All file system operations can have synchronous and asynchronous forms depending upon user requirements. To use this File System module, use the const fs = require('fs'); method
 
@@ -46,10 +46,10 @@ import * as fs from 'fs';
 
 fs.writeFile('note.txt', 'Hello from callback!', (err) => {
   if (err) {
-    console.error('❌ Error:', err);
+    console.error(' Error:', err);
     return;
   }
-  console.log('✅ File written (callback)');
+  console.log('File written (callback)');
 });
 
 Promise-based with async/await + try...catch (modern)
@@ -57,8 +57,12 @@ import { writeFile } from 'fs/promises';
 
 try {
   await writeFile('note.txt', 'Hello from promise!');
-  console.log('✅ File written (promise)');
+  console.log('File written (promise)');
 } catch (err) {
-  console.error('❌ Error:', err);
+  console.error('Error:', err);
 }
 
+
+# Day 3
+1. Create http server using node.js without express js. code added in app.js
+2. install express and create server with this (npm install express)
--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 //## below to for use module system of javascript 
+
 /*
 basic javascript
 //const add = require('./math'); 
 //console.log("Sum is:", add(10, 3));
 */
+
+// ES6 with single function exist  javascript
+
 import add from './math.js';
 console.log("Sum is:", add(10, 3));
 
 
 // ES6 with multiple function exist  javascript
+
 import {greetd, nodeHello} from './greet.js';
 console.log(greetd("mansha singhal"));
 console.log(nodeHello("singhal"));
 
-// file system
+// file system */
 import { writeFile } from 'fs/promises';
 await writeFile('hello.txt', 'Hello from Node.js!');
 
 //read file
 import { readFile } from 'fs/promises'; 
 const data = await readFile('hello.txt', 'utf-8');
-console.log('📄 File content:', data);
+console.log('File content:', data);
 
 //append text into file
 import { appendFile } from 'fs/promises';
@@ -31,10 +36,34 @@ await appendFile('hello.txt', '\nThis is a new line.');
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);//E:\nodejs\node-learning\app.js
-const __dirname = path.dirname(__filename); //E:\nodejs\node-learning
-const fullPath = path.join(__dirname,'files','myfile.txt');
+const __filename    = fileURLToPath(import.meta.url);//E:\nodejs\node-learning\app.js
+const __dirname     = path.dirname(__filename); //E:\nodejs\node-learning
+const fullPath      = path.join(__dirname,'files','myfile.txt');
 await writeFile(fullPath, 'Hello from path module!');
 
 console.log(fullPath);//:\nodejs\node-learning\files\myfile.txt
- 
+
+const result = path.resolve('folder', 'file.txt');
+console.log(result); // e.g., '/Users/me/project/folder/file.txt'
+
+
+
+//# create http server using node js
+
+import http from 'http';
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Welcome to Home Page</h1>');
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('About Us Page');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
